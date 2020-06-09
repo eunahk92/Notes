db9d46afd441b38b2425ee0c553d23838470e009
Eunah Kim <Eunahkim92@gmail.com>
2020-06-08 23:01:27 -0400

    Require Express, establish ports and routes for html pages

---
+++ server.js
@@ -0,0 +1,26 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes for pages
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+  
+app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// Listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
+  
