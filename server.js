315f315a3fc424ffdc6eee66a7aea97e87e0c3f1
Eunah Kim <Eunahkim92@gmail.com>
2020-06-10 01:56:22 -0400

    Feat post and delete request

--- server.js
+++ server.js
@@ -27,6 +27,34 @@ app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
+app.post("/api/notes", function (req, res) {
+    let note = req.body;
+    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", function(err, data) {
+        let notesArr = JSON.parse(data);
+        notesArr.push(note);
+
+        fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(notesArr), function(err, data) {
+            return notesArr;
+        });
+    });
+});
+
+app.delete('/api/notes/:id', function (req, res) {
+    let id = req.params.id;
+    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", function(err, data) {
+        let notesArr = JSON.parse(data);
+        for (let i = 0; i < notesArr.length; i++) {
+            if (notesArr[i].id === id) {
+                notesArr.splice(i, 1);
+                fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(notesArr), function(err, data) {
+                    return notesArr;
+                });
+            }
+        } 
+    });
+    res.sendFile(path.join(__dirname, "notes.html"));
+});
+
 // Listener
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
