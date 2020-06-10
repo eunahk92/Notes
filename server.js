26f24862d9282ba2e85a8d8ba6b712312dae87ff
Eunah Kim <Eunahkim92@gmail.com>
2020-06-10 01:00:43 -0400

    Feat static folder and update get methods

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 // Dependencies
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
 
 // Sets up the Express App
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+let PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/assets", express.static('./assets/'));
 
 // Routes for pages
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "notes.html"));
 });
-  
-app.get("/*", function (req, res) {
+
+app.get("/api/notes", function (req, res) {
+    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", function(err, data) {
+        return res.json(JSON.parse(data));
+    });
+});
+
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
@@ -23,4 +31,3 @@ app.get("/*", function (req, res) {
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
-  
