15f9bd12f76fe94af76011c598fac362bec02506
Eunah Kim <Eunahkim92@gmail.com>
2020-06-10 20:32:34 -0400

    Refactor to dry up code and change functions to arrow functions

--- assets/js/api-routes.js
+++ assets/js/api-routes.js
@@ -1,32 +1,27 @@
 const path = require("path");
 const fs = require("fs");
+const notesDB = require("../../db/db.json");
 
-module.exports = function(app) {
-    app.post("/api/notes", function (req, res) {
-        let note = req.body;
-        fs.readFile(path.join(__dirname, "../../db/db.json"), "utf8", function(err, data) {
-            let notesArr = JSON.parse(data);
-            notesArr.push(note);
-    
-            fs.writeFile(path.join(__dirname, "../../db/db.json"), JSON.stringify(notesArr), function(err, data) {
-                return res.json(notesArr);
-            });
+module.exports = app => {
+    app.post("/api/notes", (req, res) => {
+        notesDB.push(req.body);
+        fs.writeFile(path.join(__dirname, "../../db/db.json"), JSON.stringify(notesDB), (err, data) => {
+            if (err) throw err;
+            return res.json(notesDB);
         });
     });
     
-    app.delete('/api/notes/:id', function (req, res) {
-        let id = req.params.id;
-        fs.readFile(path.join(__dirname, "../../db/db.json"), "utf8", function(err, data) {
-            let notesArr = JSON.parse(data);
-            for (let i = 0; i < notesArr.length; i++) {
-                if (notesArr[i].id === id) {
-                    notesArr.splice(i, 1);
-                    fs.writeFile(path.join(__dirname, "../../db/db.json"), JSON.stringify(notesArr), function(err, data) {
-                        return notesArr;
-                    });
-                }
-            } 
-        });
+    app.delete('/api/notes/:id', (req, res) => {
+        const id = req.params.id;
+        for (let i = 0; i < notesDB.length; i++) {
+            if (notesDB[i].id === id) {
+                notesDB.splice(i, 1);
+                fs.writeFile(path.join(__dirname, "../../db/db.json"), JSON.stringify(notesDB), (err, data) => {
+                    if (err) throw err;
+                    return notesDB;
+                });
+            }
+        } 
         res.sendFile(path.join(__dirname, "../html/notes.html"));
     });
 }
