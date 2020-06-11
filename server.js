7d6ba81495d9e07e99edcd7cdca9d692a6947684
Eunah Kim <Eunahkim92@gmail.com>
2020-06-10 23:16:19 -0400

    Edit console log message for port listener

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ require("./assets/js/html-routes.js")(app);
 require("./assets/js/api-routes.js")(app);
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log(`App listening on:  http://localhost:${PORT}`);
 });
