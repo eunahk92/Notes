eec26f060e6a9cb06cc7ccc245956181a13f1398
Eunah Kim <Eunahkim92@gmail.com>
2020-06-10 23:15:47 -0400

    Feat a function that generates a random id number for each new note and apply it into post method

--- assets/js/api-routes.js
+++ assets/js/api-routes.js
@@ -4,17 +4,19 @@ const notesDB = require("../../db/db.json");
 
 module.exports = app => {
     app.post("/api/notes", (req, res) => {
-        notesDB.push(req.body);
+        let note = req.body;
+        note.id = createRandomID();
+        notesDB.push(note);
         fs.writeFile(path.join(__dirname, "../../db/db.json"), JSON.stringify(notesDB), (err, data) => {
             if (err) throw err;
             return res.json(notesDB);
         });
     });
-    
+
     app.delete('/api/notes/:id', (req, res) => {
         const id = req.params.id;
         for (let i = 0; i < notesDB.length; i++) {
-            if (notesDB[i].id === id) {
+            if (notesDB[i].id == id) {
                 notesDB.splice(i, 1);
                 fs.writeFile(path.join(__dirname, "../../db/db.json"), JSON.stringify(notesDB), (err, data) => {
                     if (err) throw err;
@@ -24,4 +26,15 @@ module.exports = app => {
         } 
         res.sendFile(path.join(__dirname, "../html/notes.html"));
     });
+
+    createRandomID = () => {
+        let id = '';
+        const numbers = "0123456789"
+        for (let i = 0; i < 10; i++) {
+            let index = Math.floor(Math.random() * numbers.length);
+            id += numbers.charAt(index) + "";
+            console.log(id);
+        }
+        return id;
+    };
 }
